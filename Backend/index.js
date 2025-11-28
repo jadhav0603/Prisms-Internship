c76c4e7f3093185e57e1e270b2f8fcf7947dab30
vijay0603 <jadhavvijay0808@gmail.com>
2025-11-29 00:22:50 +0530

    update cors

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://prisms-internship-frontend.onrender.com"],
+    origin: ["http://localhost:5173", "https://prisms-internship.onrender.com","https://prisms-internship-wygp.onrender.com"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true
 }));
