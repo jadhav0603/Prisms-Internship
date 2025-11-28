af30671cbb7a45e7edaa151d9fc21172520b93e2
vijay0603 <jadhavvijay0808@gmail.com>
2025-11-28 23:43:05 +0530

    port

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ const loginRoute = require('./Routes/login')
 app.use('/',loginRoute)
 
 
-app.listen(process.env.PORT,async ()=>{
+app.listen(process.env.PORT || 5000,async ()=>{
     try {
         await mongoose.connect(process.env.MONGO_URL)
         try {
